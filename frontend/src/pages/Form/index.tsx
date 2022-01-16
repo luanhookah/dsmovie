477ab38b7d4d184067a30dcedb3519593feadb3e
luanhookah <tibiaotzika@gmail.com>
2022-01-16 17:21:20 -0300

    navBar

--- frontend/src/pages/Form/index.tsx
+++
@@ -1,8 +0,0 @@
-function Form() {
-
-    return (
-        <h1>Página de formulario</h1>
-    );
- }
- 
- export default Form;
--- frontend/src/pages/Listing/index.tsx
+++
@@ -1,8 +0,0 @@
-function listing() {
-
-    return (
-        <h1>pagina de listagem</h1>
-    );
-}
-
-export default listing;
